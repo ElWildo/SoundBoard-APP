75388293c977d71fe4282cd176bdbb2a4ab8e84f
Francesco Wild <francescowild90@gmail.com>
2019-07-24 18:03:22 +0100

    edit on soundlist

--- assets/sounds/soundsList.js
+++ assets/sounds/soundsList.js
@@ -1,15 +1,7 @@
 const soundsList = [
   {
-    name: "crematorio",
-    link: require("./crematorio.mp3")
-  },
-  {
-    name: "IL RITORNO DI 'CREMATORIO'   SECONDA PARTE",
-    link: require("./il_ritorno_parte_2.mp3")
-  },
-  {
-    name: "IL RITORNO DI 'CREMATORIO' - PRIMA PARTE",
-    link: require("./il_ritorno_parte_1.mp3")
+    name: "name displayed",
+    link: require("./link.file")
   }
 ];
 
